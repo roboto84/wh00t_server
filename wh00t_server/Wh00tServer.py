f0d79817ca60bd6df4ff96abf2e5274db66f33ac
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-10 08:33:37 -0500

    feat(logging): introduce basic logging, .env file
    Create logging configuration which outputs to console
    and to a rotating log file in the log directory.  Basic error
    handling of exceptions is enhanced through this feature. Also
    introduce .env file support to configure server port.

--- wh00t_server/Wh00tServer.py
+++ wh00t_server/Wh00tServer.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 # wh00t chat server
 
+import ntpath
 import os
 import random
 import time
+import logging.config
+from dotenv import load_dotenv
 from datetime import datetime
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 from bin.Data import handles
+from wh00t_server import __version__
 
 
 class Wh00tServer:
-    SERVER_VERSION = '2.0.0'
+    SERVER_VERSION = __version__
     HOST = ''
     BUFFER_SIZE = 1024
     EXIT_STRING = '/exit'
@@ -23,7 +27,10 @@ class Wh00tServer:
     handleOptions = handles()
     messageHistory = []
 
-    def __init__(self, port):
+    def __init__(self, logging_object, home_path, port):
+        self.home_path = home_path
+        self.logger = logging_object.getLogger(type(self).__name__)
+        self.logger.setLevel(logging.INFO)
         self.address = (self.HOST, port)
 
     def run(self):
@@ -31,17 +38,17 @@ class Wh00tServer:
             self.server = socket(AF_INET, SOCK_STREAM)
             self.server.bind(self.address)
             self.server.listen(5)
-            print('Server v{} Waiting for connection...'.format(self.SERVER_VERSION))
+            self.logger.info('Server v{} Waiting for connection...'.format(self.SERVER_VERSION))
             accept_thread = Thread(target=self.accept_incoming_connections)
             accept_thread.start()
             accept_thread.join()
             self.server.close()
-        except OSError as e:
-            print('Received an OSError', e)
+        except OSError as os_error:
+            self.logger.error('Received an OSError: {}'.format(str(os_error)))
             self.server.close()
-            os._exit(1)
+            exit()
         except KeyboardInterrupt:
-            print('Received a KeyboardInterrupt... now exiting')
+            self.logger.warning('Received a KeyboardInterrupt... now exiting')
             self.server.close()
             os._exit(1)
 
@@ -57,7 +64,7 @@ class Wh00tServer:
                 client, client_address = self.server.accept()
                 connected_text = '~ You are connected to server v{}... as {} ~'.format(self.SERVER_VERSION, user_handle)
                 intro_help_message = '\n~ Type \'{}\' or press ESC key to exit ~'.format(self.EXIT_STRING)
-                print('{}:{} has connected as {}.'.format(client_address[0], client_address[1], user_handle))
+                self.logger.info('{}:{} has connected as {}.'.format(client_address[0], client_address[1], user_handle))
                 client.send(bytes(connected_text, 'utf8'))
                 client.send(bytes(intro_help_message, 'utf8'))
 
@@ -67,11 +74,12 @@ class Wh00tServer:
                 self.addresses[client] = client_address
                 Thread(target=self.handle_client, args=(client, user_handle)).start()
             except IOError as io_error:
-                print("Received IOError for {}: ".format(user_handle), io_error)
+                self.logger.warning("Received IOError for {}: ".format(user_handle), str(io_error))
                 self.handle_client_exit(client, user_handle)
                 break
             except ConnectionResetError as connection_reset_error:
-                print("Received ConnectionResetError for {}: ".format(user_handle), connection_reset_error)
+                self.logger.warning("Received ConnectionResetError for {}: ".format(user_handle),
+                                    connection_reset_error)
                 self.handle_client_exit(client, user_handle)
                 break
 
@@ -90,28 +98,40 @@ class Wh00tServer:
                     self.handle_client_exit(client, user_handle)
                     break
             except IOError as io_error:
-                print("Received IOError for {}: ".format(user_handle), io_error)
+                self.logger.warning("Received IOError for {}: ".format(user_handle), str(io_error))
                 self.handle_client_exit(client, user_handle)
                 break
             except ConnectionResetError as connection_reset_error:
-                print("Received ConnectionResetError for {}: ".format(user_handle), connection_reset_error)
+                self.logger.warning("Received ConnectionResetError for {}: ".format(user_handle),
+                                    connection_reset_error)
                 self.handle_client_exit(client, user_handle)
                 break
 
     def handle_client_exit(self, client, user_handle):
         client.close()
         del self.clients[client]
-        print('%s has disconnected.' % user_handle)
+        self.logger.info('{} has disconnected.'.format(user_handle))
         self.broadcast(bytes('\n~ {} has left the chat at {} ~'.format(user_handle, self.message_time()), 'utf8'))
 
     def broadcast(self, message, prefix=''):
         for sock in self.clients:
             sock.send(bytes(prefix, 'utf8') + message)
         self.messageHistory.append(prefix + message.decode('utf-8'))
-        if len(self.messageHistory) > 30:
+        if len(self.messageHistory) > 35:
             self.messageHistory.pop(0)
 
 
 if __name__ == '__main__':
-    wh00t_server = Wh00tServer(3001)
-    wh00t_server.run()
+    HOME_PATH = ntpath.dirname(__file__)
+    logging.config.fileConfig(fname=os.path.join(HOME_PATH, 'bin/logging.conf'), disable_existing_loggers=False)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    try:
+        load_dotenv()
+        SERVER_PORT = int(os.getenv('SERVER_PORT'))
+        wh00t_server = Wh00tServer(logging, HOME_PATH, SERVER_PORT)
+        wh00t_server.run()
+    except TypeError as type_error:
+        logger.error('Received TypeError: Check that the .env project file is configured correctly')
+        exit()
