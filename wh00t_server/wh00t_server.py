5dc053db6006a89225257dab02d1daadf40b976b
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-12-05 06:50:52 -0500

    fix(message_object): only send destructible message to chat users
    Existing destructible messages are sent to chat users and bots.
    Fix to only send these special messages to chat users only.
    
    Closes #3

--- wh00t_server/wh00t_server.py
+++ wh00t_server/wh00t_server.py
@@ -24,7 +24,6 @@ class Wh00tServer:
     _network_utils: NetworkUtils = NetworkUtils()
     _network_commons: NetworkCommons = NetworkCommons()
     _clients: dict = {}
-    _addresses: dict = {}
     _messageHistory: List[str] = []
 
     def __init__(self, logging_object: Any, port: int):
@@ -56,26 +55,29 @@ class Wh00tServer:
 
     def _accept_incoming_connections(self) -> None:
         while True:
-            user_handle: str = random.choice(self._handleOptions)
             client: Optional[socket] = None
+            init_client_info: dict = {
+                'handle': random.choice(self._handleOptions),
+                'profile': 'init:user',
+                'ip_address': '0.0.0.0'
+            }
             try:
-                client, client_address = self._server.accept()
-                self._addresses[client]: Any = client_address
-                self._logger.info(f'{client_address[0]}:{client_address[1]} has connected as {user_handle}.')
+                client, init_client_info['ip_address'] = self._server.accept()
+                self._logger.info(f'{init_client_info["ip_address"]} has connected as {init_client_info["handle"]}.')
                 self._broadcast(self._server_package('debug:broadcast_intro',
-                                                     f'~ {self._addresses[client]} has connected'
+                                                     f'~ {init_client_info["ip_address"]} has connected'
                                                      f' at {NetworkUtils.message_time()} ~'))
-                Thread(target=self._handle_client, args=(client, user_handle)).start()
+                Thread(target=self._handle_client, args=(client, init_client_info)).start()
             except IOError as io_error:
                 self._logger.warning(f'Received IOError: {str(io_error)}')
-                self._handle_client_exit(client, user_handle)
+                self._handle_client_exit(client, init_client_info)
                 break
             except ConnectionResetError as connection_reset_error:
                 self._logger.warning(f'Received ConnectionResetError: {str(connection_reset_error)}')
-                self._handle_client_exit(client, user_handle)
+                self._handle_client_exit(client, init_client_info)
                 break
 
-    def _handle_client(self, client: socket, user_handle: str) -> None:
+    def _handle_client(self, client: socket, init_client_info: dict) -> None:
         while True:
             try:
                 package: str = self._network_utils.unpack_byte(client.recv(self._network_commons.get_buffer_size()))
@@ -86,89 +88,99 @@ class Wh00tServer:
                     package_dict_list: List[dict] = NetworkUtils.unpack_data(package)
                     for package_dict in package_dict_list:
                         if package_dict['message'] == '':
-                            new_user_handle: str = package_dict['id']
-                            client_is_app: bool = package_dict['profile'] == self._network_commons.get_app_profile()
+                            new_client_handle: str = package_dict['id']
+                            new_client_profile: str = package_dict['profile']
+                            message_category: str = 'broadcast_intro'
+                            client_is_app: bool = new_client_profile == self._network_commons.get_app_profile()
                             if client_is_app:
-                                message_category: str = 'debug:broadcast_intro'
-                            else:
-                                message_category: str = 'broadcast_intro'
+                                message_category: str = f'debug:{message_category}'
                             self._broadcast(self._server_package(message_category,
-                                                                 f'~ {new_user_handle} has connected'
+                                                                 f'~ {new_client_handle} has connected'
                                                                  f' at {NetworkUtils.message_time()} ~'))
-                            self._clients[client]: str = new_user_handle
+                            self._clients[client]: dict = {
+                                'handle': new_client_handle,
+                                'profile': new_client_profile,
+                                'ip_address': init_client_info['ip_address']
+                            }
                             self._logger.info(
-                                f'{self._addresses[client]}:{user_handle} is now {self._clients[client]}.')
-                            client.send(self._server_package('client_intro',
-                                                             f'~ You are connected to wh00t server '
-                                                             f'v{self._SERVER_VERSION}... '
-                                                             f'as {self._clients[client]} ~'))
+                                f'{init_client_info["ip_address"]}:{init_client_info["handle"]} '
+                                f'is now {self._clients[client]["handle"]}.')
+                            client.send(self._network_utils.utf8_bytes(
+                                self._server_package('client_intro',
+                                                     f'~ You are connected to wh00t server '
+                                                     f'v{self._SERVER_VERSION}... '
+                                                     f'as {self._clients[client]["handle"]} ~')))
                             if not client_is_app:
                                 self._client_intro_message_history(client, self._messageHistory)
                         elif package_dict['message'] == self._network_commons.get_exit_command():
-                            client.send(self._server_package('client_exit', self._network_commons.get_exit_command()))
-                            self._handle_client_exit(client, self._clients[client], package_dict['profile'])
+                            client.send(self._network_utils.utf8_bytes(
+                                self._server_package('client_exit', self._network_commons.get_exit_command())))
+                            self._handle_client_exit(client, self._clients[client])
                             return
                         else:
-                            self._broadcast(self._network_utils.utf8_bytes(NetworkUtils.package_dict(package_dict)))
+                            self._broadcast(NetworkUtils.package_dict(package_dict))
             except SyntaxError as syntax_error:
-                self._logger.warning(f'Received SyntaxError for {self._clients[client]}: '
+                self._logger.warning(f'Received SyntaxError for {self._clients[client]["handle"]}: '
                                      f'{str(syntax_error)}')
                 self._handle_client_exit(client, self._clients[client])
                 break
             except IOError as io_error:
-                self._logger.warning(f'Received IOError for {self._clients[client]}: {str(io_error)}')
+                self._logger.warning(f'Received IOError for {self._clients[client]["handle"]}: {str(io_error)}')
                 self._handle_client_exit(client, self._clients[client])
                 break
             except ConnectionResetError as connection_reset_error:
-                self._logger.warning(f'Received ConnectionResetError for {self._clients[client]}: '
+                self._logger.warning(f'Received ConnectionResetError for {self._clients[client]["handle"]}: '
                                      f'{str(connection_reset_error)}')
                 self._handle_client_exit(client, self._clients[client])
                 break
 
-    def _handle_client_exit(self, client: socket, user_handle: str, client_profile: Optional[str] = '') -> None:
+    def _handle_client_exit(self, client: socket, client_info: dict) -> None:
         client.close()
         del self._clients[client]
-        self._logger.info(f'{user_handle} has disconnected.')
-        if client_profile and client_profile == self._network_commons.get_user_profile():
+        self._logger.info(f'{client_info["handle"]} has disconnected.')
+        if client_info['profile'] and client_info['profile'] == self._network_commons.get_user_profile():
             self._broadcast(self._server_package('broadcast_exit',
-                                                 f'~ {user_handle} has left the chat at '
+                                                 f'~ {client_info["handle"]} has left the chat at '
                                                  f'{NetworkUtils.message_time()} ~'))
 
-    def _broadcast(self, message_package: bytes) -> None:
+    def _broadcast(self, message_package: str) -> None:
+        unpacked_package: dict = self._network_utils.unpack_data(message_package)[0]
+        secret_message: bool = self._secret_message(unpacked_package['profile'], unpacked_package['message'])
         for sock in self._clients:
-            sock.send(message_package)
-        self._add_to_history(self._network_utils.unpack_data(self._network_utils.unpack_byte(message_package))[0])
+            if not secret_message or (secret_message and
+                                      self._clients[sock]['profile'] == self._network_commons.get_user_profile()):
+                sock.send(self._network_utils.utf8_bytes(message_package))
+        if not secret_message and unpacked_package['profile'] == self._network_commons.get_user_profile():
+            self._add_to_history(self._network_utils.unpack_data(message_package)[0])
 
     def _add_to_history(self, package_dict: dict) -> None:
-        client_profile: str = package_dict['profile']
         max_message_history: int = 35
-        if client_profile and client_profile == self._network_commons.get_user_profile() and \
-                not self._self_destruct(package_dict['message']):
-            self._messageHistory.append(NetworkUtils.package_dict(package_dict))
-            if len(self._messageHistory) > max_message_history:
-                self._messageHistory.pop(0)
+        self._messageHistory.append(NetworkUtils.package_dict(package_dict))
+        if len(self._messageHistory) > max_message_history:
+            self._messageHistory.pop(0)
 
     def _client_intro_message_history(self, client: socket, message_history: List[str]) -> None:
         if len(message_history) > 0:
             counter: int = 1
             message_category: str = 'message_history'
-            client.send(self._server_package(message_category, f'~~~ history start ~~~'))
+            client.send(self._network_utils.utf8_bytes(self._server_package(message_category, f'~ history start ~')))
             for historical_message in message_history:
                 if counter % 5 == 0:
                     time.sleep(.9)
                 client.send(self._network_utils.utf8_bytes(historical_message))
                 counter += 1
                 time.sleep(.1)
-            client.send(self._server_package(message_category, f'~~~ history end ~~~'))
+            client.send(self._network_utils.utf8_bytes(self._server_package(message_category, f'~ history end ~')))
 
-    def _server_package(self, message_category: str, message: str) -> bytes:
-        return self._network_utils.byte_package(self._network_commons.get_server_id(),
+    def _server_package(self, message_category: str, message: str) -> str:
+        return self._network_utils.package_data(self._network_commons.get_server_id(),
                                                 self._network_commons.get_app_profile(),
                                                 message_category,
                                                 message)
 
-    def _self_destruct(self, message) -> bool:
-        return message.find(self._network_commons.get_destruct_command()) != -1
+    def _secret_message(self, client_profile: str, message: str) -> bool:
+        return client_profile == self._network_commons.get_user_profile() and \
+               self._network_commons.get_destruct_command() in message
 
 
 if __name__ == '__main__':
